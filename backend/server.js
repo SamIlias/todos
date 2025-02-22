ed0a4e4905d04d493da92588df1bc83949b2a206
SamIlias <Samovichilias19life@gmail.com>
2025-02-22 13:49:42 +0300

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,54 @@
+import express from 'express';
+import fsp from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import cors from 'cors';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = 3000;
+const filePath = path.join(__dirname, 'todos.json');
+
+app.use(cors()); // allow CORS so that frontend can make requests
+app.use(express.json());
+
+// POST /todos — save todo
+app.post('/todos', async (req, res) => {
+  try {
+    const data = req.body; //{ todo: {name: 'name'}}
+
+    let todos = [];
+    try {
+      const fileData = await fsp.readFile(filePath, 'utf-8');
+      todos = JSON.parse(fileData);
+    } catch (error) {
+      console.log(' File does not exist. Create new file.');
+    }
+
+    todos.push(data.todo.name);
+
+    await fsp.writeFile(filePath, JSON.stringify(todos, null, 2));
+
+    res.status(201).json({ message: 'Todo has saved!', todos });
+  } catch (error) {
+    console.error('An error occured while saving.', error);
+    res.status(500).json({ message: 'Server error.' });
+  }
+});
+
+// GET /todos — получить все задачи
+app.get('/todos', async (req, res) => {
+  try {
+    const fileData = await fsp.readFile(filePath, 'utf-8');
+    const todos = JSON.parse(fileData);
+    res.json(todos);
+  } catch (error) {
+    res.status(200).json([]); // return empty array if file doesn't exist
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
